afbf18a7d702f2b23eeb71ef9314b2896c0a2166
Roman_Tretiakov <romantre@wizardsdev.com>
2024-06-24 15:46:47 +0300

    Add HW2

---
+++ homework2.py
@@ -0,0 +1,7 @@
+home_work_quantity = 12
+spent_hour = 1.5
+course_name = 'Python'
+average_spent_time = spent_hour / home_work_quantity
+
+print('Курс:', f'{course_name},', 'всего задач:', f'{home_work_quantity},',
+      'затрачено часов:', f'{spent_hour},', 'среднее время выполнения', average_spent_time, 'часа.')
